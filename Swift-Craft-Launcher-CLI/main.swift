f928e004a5825febb610e96a5a67482484432bef
hjy_666 <hjy_233@qq.com>
2026-02-26 14:10:09 +0800

    Add ? alias for help

--- Swift-Craft-Launcher-CLI/main.swift
+++ Swift-Craft-Launcher-CLI/main.swift
@@ -4,6 +4,7 @@ import Darwin
 var args = Array(CommandLine.arguments.dropFirst())
 jsonOutputEnabled = args.contains("--json")
 args.removeAll(where: { $0 == "--json" })
+args = args.map { $0 == "?" ? "--help" : $0 }
 
 if args.isEmpty || args == ["--help"] || args == ["-h"] || args == ["help"] {
     printGlobalHelp()
